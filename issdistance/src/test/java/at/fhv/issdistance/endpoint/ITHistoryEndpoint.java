a81dd03c69ceb2329cd3c093172c82145d1c9e6d
tscholy <cts6638@students.fhv.at>
2019-01-30 15:20:51 +0100

    added one integration test

--- issdistance/src/test/java/at/fhv/issdistance/endpoint/ITHistoryEndpoint.java
+++ issdistance/src/test/java/at/fhv/issdistance/endpoint/ITHistoryEndpoint.java
@@ -1,4 +1,23 @@
 package at.fhv.issdistance.endpoint;
 
+import at.fhv.issdistance.endpoints.AnalysisEndpoint;
+import at.fhv.issdistance.endpoints.AuthEndpoint;
+import at.fhv.issdistance.handlers.HistoryHandler;
+import at.fhv.issdistance.models.DistanceHistory;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ITHistoryEndpoint {
+
+    @Test
+    public void testHistory() throws Exception {
+        AuthEndpoint authEndpoint = new AuthEndpoint();
+        AnalysisEndpoint analysisEndpoint = new AnalysisEndpoint();
+        String token = authEndpoint.login("username1");
+        analysisEndpoint.analyze(token);
+        HistoryHandler historyHandler = HistoryHandler.getInstance();
+        DistanceHistory distanceHistory = historyHandler.getHistory(token);
+        Assert.assertNotNull(distanceHistory);
+    }
+
 }
