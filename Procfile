e167e7a548176d9d2209c30f0f2392c7779c2410
tscholy <cts6638@students.fhv.at>
2019-01-31 10:28:49 +0100

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web java -jar target/issdistance-0.0.1-SNAPSHOT.jar
--- src/test/java/at/fhv/issdistance/IssdistanceApplicationTests.java
+++ src/test/java/at/fhv/issdistance/IssdistanceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = IssdistanceApplicationTests.class)
 public class IssdistanceApplicationTests {
 
 	@Test
